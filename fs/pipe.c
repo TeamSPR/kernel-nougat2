e4ef8ac12931814ba74ddc0c0004e90a5e7fb081
UpInTheAir <upintheair.xda@gmail.com>
2015-08-28 11:02:44 +0700

    Linux 3.10.82
    Signed-off-by: UpInTheAir <upintheair.xda@gmail.com>

--- fs/pipe.c
+++ fs/pipe.c
@@ -421,10 +421,9 @@ pipe_read(struct kiocb *iocb, const struct iovec *_iov,
 
 			atomic = !iov_fault_in_pages_write(iov, chars);
 			remaining = chars;
-			offset = buf->offset;
 redo:
 			addr = ops->map(pipe, buf, atomic);
-			error = pipe_iov_copy_to_user(iov, addr, &offset,
+			error = pipe_iov_copy_to_user(iov, addr, &buf->offset,
 						      &remaining, atomic);
 			ops->unmap(pipe, buf, addr);
 			if (unlikely(error)) {
