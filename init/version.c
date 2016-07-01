123ef6301a32f17d3545ef22209d01ee559ed9c5
Tom Hite <tdhite@gmail.com>
2016-06-30 23:08:53 -0500

    version: change to reflect modern reality.

--- init/version.c
+++ init/version.c
@@ -41,10 +41,10 @@ EXPORT_SYMBOL_GPL(init_uts_ns);
 
 /* FIXED STRINGS! Don't touch! */
 const char linux_banner[] =
-	"Linux version " UTS_RELEASE " (UpInTheAir)"
+	"Linux version " UTS_RELEASE " (tdhite)"
 	"(" LINUX_COMPILER ") " UTS_VERSION "\n";
 
 const char linux_proc_banner[] =
 	"%s version %s"
-	" (UpInTheAir)"
+	" (tdhite)"
 	" (" LINUX_COMPILER ") %s\n";
