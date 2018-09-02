3832f09640fee74fcf79fb7abd1b969f5fa1a57a
Tom Hite <tdhite@gmail.com>
2018-09-02 15:10:21 -0500

    config: sync up to date

--- drivers/cpufreq/cpufreq_smartmax_eps.c
+++ drivers/cpufreq/cpufreq_smartmax_eps.c
@@ -88,7 +88,7 @@ extern int tegra_input_boost (struct cpufreq_policy *policy,
 #endif
 
 // msm8974 platform
-#ifdef CONFIG_CPU_FREQ_GOV_SMARTMAX_EPS
+//#ifdef CONFIG_CPU_FREQ_GOV_SMARTMAX_EPS
 #define DEFAULT_SUSPEND_IDEAL_FREQ 300000
 #define DEFAULT_AWAKE_IDEAL_FREQ 652800
 #define DEFAULT_RAMP_UP_STEP 200000
@@ -103,7 +103,7 @@ extern int tegra_input_boost (struct cpufreq_policy *policy,
 #define DEFAULT_BOOST_FREQ 1497600
 #define DEFAULT_IO_IS_BUSY 0
 #define DEFAULT_IGNORE_NICE 1
-#endif
+//#endif
 
 #ifdef CONFIG_CPU_FREQ_GOV_SMARTMAX_EPS_PRIMOU
 #define DEFAULT_SUSPEND_IDEAL_FREQ 368000
